dbef0300671c84f194cd14fc22ce05d5c26346b0
John Wiesenfeld <jawiesenfeld@gmail.com>
2019-03-20 14:13:02 -0700

    added balance factor/height calculations to AVL. Still need to write rotations

--- AVL.java
+++ AVL.java
@@ -6,13 +6,15 @@ public class AVL extends BST
 
 	public AVL(BST other)
 	{
-		Vector<Node> tempStorage = new Vector<Node>();
-
 		//temporarily store nodes sorted in Vector via inOrder walk of BST
+		Vector<Node> tempStorage = new Vector<Node>();
 		StoreNodes(other, tempStorage);
 
 		//build balanced tree from Vector, by recursively inserting mid-point of vector via pre-order
 		BuildBalancedTree(0, tempStorage.size()-1, tempStorage);
+
+		//Update heights in each Node of new AVL tree
+		UpdateHeights(root);
 	}
 
 //helper methods for contstructing AVL from any BST
@@ -49,10 +51,26 @@ public class AVL extends BST
 //AVL insertion
 	public void Insert(String Key, boolean[] InFile)
 	{
+		//normal BST insertion
 		super.Insert(Key, InFile);
+
 		//Do AVL Stuff
 		Node temp = getNode(Key);
-		//...
+		while(temp != null)
+		{
+			if(BalanceFactor(temp) < -1)
+			{
+				//...
+				UpdateHeights(temp);
+				break;
+			} else if(BalanceFactor(temp) > 1)
+			{
+				//...
+				UpdateHeights(temp);
+				break;
+			}
+			temp = temp.GetParent();
+		}
 	}
 
 
@@ -60,8 +78,37 @@ public class AVL extends BST
 	public void Delete(String Key, int FileNumber)
 	{
 		super.Delete(Key, FileNumber);
-		//???? Do AVL Stuff?
+		//???? Do AVL Stuff? or are we doing lazy deletions?
 		//...
 	}
 
+//BalanceFactor Calculation
+	private int BalanceFactor(Node node)
+	{
+		return node.GetLeft().GetHeight() - node.GetRight().GetHeight();
+	}
+
+	private int Height(Node node)
+	{
+		if (node == null) {
+			return 0;
+		}
+
+		int heightLeft = Height(node.GetLeft());
+		int heightRight = Height(node.GetRight());
+
+		return heightLeft > heightRight ? heightLeft + 1 : heightRight + 1;
+	}
+
+//Update all node heights in tree/subtree with starting at 'node'
+	private void UpdateHeights(Node node)
+	{
+		if(node == null)
+		{
+			return;
+		}
+		node.SetHeight(Height(node));
+		UpdateHeights(node.GetLeft());
+		UpdateHeights(node.GetRight());
+	}
 }
--- Node.java
+++ Node.java
@@ -3,7 +3,7 @@ public class Node implements Comparable<Node>
 	private String key;
 	private boolean[] inFile;
 	private Node left, right, parent;
-	private int balanceFactor;
+	private int height;
 	private static final int fileCount = 5;
 
 	public Node(String Key)
@@ -13,7 +13,7 @@ public class Node implements Comparable<Node>
 		left = null;
 		right = null;
 		parent = null;
-		balanceFactor = 0;
+		height = 0;
 	}
 	public Node(String Key, boolean[] InFile)
 	{
@@ -22,7 +22,7 @@ public class Node implements Comparable<Node>
 		left = null;
 		right = null;
 		parent = null;
-		balanceFactor = 0;
+		height = 0;
 	}
 	public Node(String Key, boolean[] InFile, Node Parent)
 	{
@@ -31,7 +31,7 @@ public class Node implements Comparable<Node>
 		left = null;
 		right = null;
 		parent = Parent;
-		balanceFactor = 0;
+		height = 0;
 	}
 	public Node(String Key, boolean[] InFile, Node Parent, Node Left, Node Right)
 	{
@@ -40,16 +40,16 @@ public class Node implements Comparable<Node>
 		left = Left;
 		right = Right;
 		parent = Parent;
-		balanceFactor = 0;
+		height = 0;
 	}
-	public Node(String Key, boolean[] InFile, Node Parent, Node Left, Node Right, int BalanceFactor)
+	public Node(String Key, boolean[] InFile, Node Parent, Node Left, Node Right, int Height)
 	{
 		key = key;
 		inFile = InFile;
 		left = Left;
 		right = Right;
 		parent = Parent;
-		balanceFactor = BalanceFactor;
+		height = Height;
 	}
 
 	public int compareTo(Node other)
@@ -89,8 +89,8 @@ public class Node implements Comparable<Node>
 	public void SetKey(String Key) { key = Key; }
 	public String GetKey() { return key; }
 
-	public void SetBalanceFactor(int BalanceFactor) { balanceFactor = BalanceFactor; }
-	public int GetBalanceFactor() { return balanceFactor; }
+	public void SetHeight(int Height) { height = Height; }
+	public int GetHeight() { return height; }
 
 	public void SetLeft(Node Left) { left = Left; }
 	public void SetRight(Node Right) { right = Right; }
